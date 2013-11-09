8cc6cdd06e0aba840cffbfb8b9171921b4a6f1a2
Chris Taylor <cctaylor@gmail.com>
2013-11-09 12:03:53 -0500

    Add precompiled assets

--- db/migrate/20130919152353_change_index_on_quote.rb
+++ db/migrate/20130919152353_change_index_on_quote.rb
@@ -1,11 +1,9 @@
 class ChangeIndexOnQuote < ActiveRecord::Migration
   def up
-  	remove_index :quotes, [:email, :created_at]
   	add_index :quotes, [:user_id, :created_at]
   end
 
   def down
   	remove_index :quotes, [:user_id, :created_at]
-  	add_index :quotes, [:email, :created_at]
   end
 end
